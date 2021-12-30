1c8774e4d197c384eb987563cab9b52cd3b8b4f6
sofia-on-air <sofia.on.air@gmail.com>
2021-12-30 20:00:57 +0300

    добавление стартового экрана

--- program.py
+++ program.py
@@ -4,6 +4,43 @@ from obstacle import Obstacle
 from field import Field
 from levelLoader import LevelLoader
 from const import *
+from loadImage import loadImage
+import sys
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen(screen):
+    intro_text = ["GO FAST", "",
+                  "Чтобы управлять игроком, необходимо,",
+                  "использовать клавиши 'вверх', 'вниз'"]
+
+    fon = pygame.transform.scale(loadImage('fon.jpg'), (WINDOW_WIDTH, WINDOW_HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(fps)
 
 
 def main():         # писала Алия(92-102)
@@ -12,6 +49,7 @@ def main():         # писала Алия(92-102)
     level_count = 3
     size = WINDOW_WIDTH, WINDOW_HEIGHT
     screen = pygame.display.set_mode(size)
+    start_screen(screen)
 
     # здесь надо показать стартовое окно с выбором уровня (переменная level)
 
