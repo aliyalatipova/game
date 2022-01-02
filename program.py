0a96728ca5f353d797425ee0f2a569438b1b3f35
sofia-on-air <sofia.on.air@gmail.com>
2022-01-03 00:14:26 +0300

    добавление экранов: начало уровня, проигрыш, выигрыш

--- program.py
+++ program.py
@@ -16,8 +16,44 @@ def terminate():
 def start_screen(screen):
     intro_text = ["GO FAST", "",
                   "Чтобы управлять игроком, необходимо,",
-                  "использовать клавиши 'вверх', 'вниз'"]
+                  "использовать клавиши 'вверх', 'вниз'.",
+                  "Нажмите [1], [2] или [3] для выбора уровня,",
+                  "либо нажмите любую кнопку для начала игры."]
 
+    fon = pygame.transform.scale(loadImage('fon.jpg'), (WINDOW_WIDTH, WINDOW_HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN and (pygame.key.get_pressed()[pygame.K_1] or pygame.key.get_pressed()[pygame.K_KP1]):
+                return 1
+            elif event.type == pygame.KEYDOWN and (pygame.key.get_pressed()[pygame.K_2] or pygame.key.get_pressed()[pygame.K_KP2]):
+                return 2
+            elif event.type == pygame.KEYDOWN and (pygame.key.get_pressed()[pygame.K_3] or pygame.key.get_pressed()[pygame.K_KP3]):
+                return 3
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return 1  # начинаем игру
+        pygame.display.flip()
+        clock.tick(fps)
+
+
+def level_start_screen(screen, level):
+    intro_text = ["Уровень " + str(level) + ".",
+                  "Нажмите любую кнопку для начала."]
     fon = pygame.transform.scale(loadImage('fon.jpg'), (WINDOW_WIDTH, WINDOW_HEIGHT))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
@@ -38,24 +74,81 @@ def start_screen(screen):
                 terminate()
             elif event.type == pygame.KEYDOWN or \
                     event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
+                return
+        pygame.display.flip()
+        clock.tick(fps)
+
+
+def lose_screen(screen):
+    # Когда сделаем жизни, здесь надо будет менять текст
+    intro_text = ["Вы проиграли.", "",
+                  "Нажмите любую кнопку, чтобы начать заново."]
+    fon = pygame.transform.scale(loadImage('fon.jpg'), (WINDOW_WIDTH, WINDOW_HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+        clock.tick(fps)
+
+
+def win_screen(screen):
+    intro_text = ["Вы выиграли!", "",
+                  "Нажмите любую кнопку, чтобы закончить."]
+    fon = pygame.transform.scale(loadImage('fon.jpg'), (WINDOW_WIDTH, WINDOW_HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return
         pygame.display.flip()
         clock.tick(fps)
 
 
 def main():         # писала Алия(92-102)
     pygame.init()
-    level = 1
     level_count = 3
     size = WINDOW_WIDTH, WINDOW_HEIGHT
     screen = pygame.display.set_mode(size)
-    start_screen(screen)
+    level = start_screen(screen)
 
     # здесь надо показать стартовое окно с выбором уровня (переменная level)
 
     hit = False
     while level <= level_count and hit == False:
-        # здесь надо показать стартовое окно уровня
+        level_start_screen(screen, level)
+
         pygame.display.set_caption('Уровень ' + str(level))
         running = True
 
@@ -103,6 +196,7 @@ def main():         # писала Алия(92-102)
                 if player.y_pos > sprite.rect.y and player.y_pos < sprite.rect.y + OBSTACLE_SIZE:
                     if player.x_pos + R >= sprite.rect.x and player.x_pos - R <= sprite.rect.x + OBSTACLE_SIZE:
                         hit = True  # обработать столкновение
+                        running = False
                         # print('hit')
                 if sprite.rect.x + OBSTACLE_SIZE < WINDOW_WIDTH // 2:
                     counter -= 1
@@ -112,9 +206,16 @@ def main():         # писала Алия(92-102)
                 running = False
 
             pygame.display.flip()
-        level += 1
+
+        if hit is True:
+            lose_screen(screen)
+            level = start_screen(screen)
+            hit = False
+        else:
+            level += 1
 
     # здесь надо показать финальное окно
+    win_screen(screen)
     pygame.quit()
 
 
